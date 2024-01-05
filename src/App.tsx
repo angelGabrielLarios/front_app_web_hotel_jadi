673eca35d13291b43cf9591f5e2d42b342a5d42c
Angel Larios <lariosacostaa@gmail.com>
2024-01-05 15:36:30 -0600

    template react app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+export const App = () => {
+    return (
+        <>
+            <main className="min-h-screen flex flex-col justify-center items-center">
+                <h1>App</h1>
+            </main>
+        </>
+    )
+}
